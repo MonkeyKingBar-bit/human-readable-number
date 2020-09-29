469539931c57b8f1910883e3a77b5c2c093fa1e7
Catherine Veremeychik <kateiiverem@gmail.com>
2020-09-29 14:16:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function toReadable (number) {
     const numWords = require('num-words')
- 
     const amountInWords = numWords(number);
     return amountInWords.replace(/\b and\b/g,'');
 }
